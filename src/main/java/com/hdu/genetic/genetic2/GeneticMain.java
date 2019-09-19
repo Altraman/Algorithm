936aabc7754e90f334c0cc89779be38a91269731
Altraman <1369012624@qq.com>
2019-09-19 15:36:44 +0800

    遗传算法计算x + 10sin(5x) + 7cos(4x), x∈[0,9]最大值

--- src/main/java/com/hdu/genetic/genetic2/GeneticMain.java
+++ src/main/java/com/hdu/genetic/genetic2/GeneticMain.java
@@ -1,6 +1,6 @@
 package com.hdu.genetic.genetic2;
 
-public class Main {
+public class GeneticMain {
     public static void main(String[] args) {
         final Algorithm ga = new Algorithm();
         final Population population = new Population();
